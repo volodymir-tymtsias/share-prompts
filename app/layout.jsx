aec9d6e8869a65d882d3e0922c90856f3532ad47
Volodymir Tymtsias <vovanuch2007@gmail.com>
2024-04-25 15:14:49 +0300

    added home page and root layout

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Share prompts',
+  description: 'Discover & Share AI Prompts',
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <div className="app">
+          {children}
+        </div>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout;
