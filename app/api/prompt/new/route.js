75318c4feac7a3fc1fd8567ed0c385e01569becc
Volodymir Tymtsias <vovanuch2007@gmail.com>
2024-04-29 16:40:12 +0300

    the functionality of adding a new prompt has been added

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,21 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+export const POST = async (req) => {
+  const { userId, prompt, tag } = await req.json();
+
+  try {
+    connectToDB();
+    const newPrompt = new Prompt({
+      creater: userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
